3b7dae330ec4e5f7b3ed0a85227a78a0ce60df50
Marcos Blánquez <marcosblanquez9@gmail.com>
2025-05-05 11:58:35 +0200

    Auto stash before merge of "marcos-kqbg" and "origin/main"

--- GestionHotel/src/Model/Eventos.java
+++ GestionHotel/src/Model/Eventos.java
@@ -1,10 +1,50 @@
 package Model;
 
+import java.util.Date;
+
 public class Eventos {
 
-    private int fecha;
+    private final int sala = 1;
+    private Date fecha;
     private int cantidad;
-    private String nombre;
-    private int habitacion;
+    private String tipo;
+    private boolean catering;
+    
+    public Eventos(Date fecha, String tipo, boolean catering) {
+        this.fecha = fecha;
+        this.tipo = tipo;
+        this.catering = catering;
+    }
+
+    public int getCantidad() {
+        return this.cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getTipo() {
+        return this.tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public boolean getCatering() {
+        return this.catering;
+    }
+    public void setCatering(boolean catering) {
+        this.catering = catering;
+    }
+
+    public Date getDate() {
+        return this.fecha;
+    }
+
+    public void setDate(Date fecha) {
+        this.fecha = fecha;
+    }
 
 }
+
